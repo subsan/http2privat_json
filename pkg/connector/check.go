e17aae37ff44b93a71b49e6b0118f2de7a0b1b4e
subsan <subsan@online.ua>
2025-12-10 15:36:02 +0200

    fix check connectino

--- pkg/connector/check.go
+++ pkg/connector/check.go
@@ -1,14 +1,7 @@
 package connector
 
 func CheckConnection() JsonEntity {
-	if !isConnected {
-		return JsonEntity{
-			Error:            true,
-			ErrorDescription: "Terminal not connected",
-		}
-	}
-
-	return SyncSender(JsonEntity{
-		Method: "PingDevice",
-	})
+    return SyncSender(JsonEntity{
+        Method: "PingDevice",
+    })
 }
